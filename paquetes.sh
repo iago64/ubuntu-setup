5d2ba7b9e46b920eff30f8ddb76bb67a11ed844c
Damian Facchini <facchinidm@gmail.com>
2021-07-26 23:24:20 -0300

    chore: add net-tools for ifconfig

--- paquetes.sh
+++ paquetes.sh
@@ -3,10 +3,10 @@ sudo apt update
 
 # Paquetes Herramientas
 echo "Installando Herramientas Generales"
-sudo apt install aptitude vim emacs nano strace ssh curl htop tree wget terminator xclip bless neofetch
+sudo apt install aptitude vim emacs nano strace ssh curl htop tree wget terminator xclip bless neofetch net-tools
 
 # Lenguajes de Programacion
-echo "Installando Lenguajes de Programacion"
+echo "Installando Lenguajes y Herramientas de Programacion"
 sudo apt install build-essential autotools-dev gcc gdb g++ python3 python3-pip openjdk-11-jdk libcunit1-dev libcunit1 make cmake
 
 # DEV Utils
