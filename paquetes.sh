8279376508718dfceead4b705d6c77042f5c4789
Damian Facchini <facchinidm@gmail.com>
2024-05-31 15:44:40 -0300

    chore: remove bless

--- paquetes.sh
+++ paquetes.sh
@@ -3,11 +3,11 @@ sudo apt update
 
 # Paquetes Herramientas
 echo "Installando Herramientas Generales"
-sudo apt install aptitude vim emacs nano strace ssh curl htop tree wget terminator xclip bless neofetch net-tools
+sudo apt install aptitude vim emacs nano strace ssh curl htop tree wget terminator xclip neofetch net-tools
 
 # Lenguajes de Programacion
 echo "Installando Lenguajes y Herramientas de Programacion"
-sudo apt install build-essential autotools-dev gcc gdb g++ python3 python3-pip openjdk-21-jdk openjdk-21-jre libcunit1-dev libcunit1 make cmake bless
+sudo apt install build-essential autotools-dev gcc gdb g++ python3 python3-pip openjdk-21-jdk openjdk-21-jre libcunit1-dev libcunit1 make cmake
 
 # DEV Utils
 echo "Installando Utilidades de Programacion"
