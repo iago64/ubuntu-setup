76f3c9c72577dd73e4e1150ae99092cb61a6ef0f
Damian Facchini <facchinidm@gmail.com>
2024-05-31 14:12:16 -0300

    feat: update to Java 21

--- paquetes.sh
+++ paquetes.sh
@@ -7,7 +7,7 @@ sudo apt install aptitude vim emacs nano strace ssh curl htop tree wget terminat
 
 # Lenguajes de Programacion
 echo "Installando Lenguajes y Herramientas de Programacion"
-sudo apt install build-essential autotools-dev gcc gdb g++ python3 python3-pip openjdk-17-jdk openjdk-17-jre libcunit1-dev libcunit1 make cmake bless
+sudo apt install build-essential autotools-dev gcc gdb g++ python3 python3-pip openjdk-21-jdk openjdk-21-jre libcunit1-dev libcunit1 make cmake bless
 
 # DEV Utils
 echo "Installando Utilidades de Programacion"
